b54d4d8173222b51e1e1ce3f8d27d40782583623
Tomás de Siervi Barcellos <tomas.imperialismoedependencia@imperialismoedependencia.org>
2017-04-15 14:10:30 -0500

    Correções ortográficas menores

--- fontes/CEPAL.R
+++ fontes/CEPAL.R
@@ -3,6 +3,9 @@ termos_troca <- CEPAL_stat(883)
 
 desemprego <- CEPAL_stat(119)
 
+desnutricao <- CEPAL_stat(164) # precisa tirar ano como num na funcao
+
+
 saveRDS(termos_troca, 'dados/termos_troca.RDS')
 saveRDS(desemprego, 'dados/desemprego.RDS')
 rm(list=ls())
